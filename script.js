675d0ad7bd0a98f76917ec26ba69a1495354e119
DanieleChecchin <danielechecchin26@gmail.com>
2024-07-31 14:03:17 +0200

    scaletta

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/* SCALETTA
+
+1- Recupero gli elementi del DOM di mio interesse
+2- Chiedo all'utente il numero di km che deve percorrere
+3- Chiedo all'utente la sua età
+4- Calcolo il prezzo del biglietto in base ai km ed all'età
+5- Stampo nel DOM
+
+*/
